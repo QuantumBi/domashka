5f5a588be59259420fa2b5014ceb9f9d8675fbca
Kamil Haidarov <ckamil.haidarov@yandex.ru>
2024-12-07 15:44:27 +0300

    modified resursion

--- recursion.py
+++ recursion.py
@@ -1,10 +1,11 @@
 def get_multiplied_digits(number):
     str_number = str(number)
+    str_number = str_number.replace("0", "1")
     first = int(str_number[0])
-    if len(str_number) > 1:
+    if len(str_number) > 1 and first != 0:
         return first * get_multiplied_digits(int(str_number[1:]))
     else:
         return first
 
-print(get_multiplied_digits(40203))
+print(get_multiplied_digits(402030))
 
