b2a57c3be5989165975c8123ad851f1294eb95ce
QuantumBi <162317196+QuantumBi@users.noreply.github.com>
2024-12-23 02:04:42 +0300

    Add files via upload

--- caesar.py
+++ caesar.py
@@ -3,34 +3,65 @@ from operator import index
 const_upper = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ' # Прописные буквы
 const_lower = 'abcdefghijklmnopqrstuvwxyz' # Строчные буквы
 
+print("""Для зашифровки фразы введите 0
+Для расшифроки фразы введите 1
+Для вывода зашифрованной фразы введите list
+Для вывода расшифрованной фразы введите postlist
+Для выхода введите end\n""")
 
-step = 1
-new_words = []
-words = input("Введите вашу для шифрования \n:")
-for i in words:
-    if i in const_upper:
-        num = const_upper.index(i)
-        new_words.append(const_upper[num-step])
-    elif i in const_lower:
-        num = const_lower.index(i)
-        new_words.append(const_lower[num - step])
-    else:
-        new_words.append(i)
 
-print("Зашифрованная фраза")
-for i in new_words:
-    print(i, end="")
-print()
-
+new_word = []
 post_word = []
-for i in new_words:
-    if i in const_upper:
-        num = const_upper.index(i)
-        post_word.append(const_upper[num+step-26])
-    elif i in const_lower:
-        num = const_lower.index(i)
-        post_word.append(const_lower[num+step-26])
-    else:
-        post_word.append(i)
-for i in post_word:
-    print(i, end="")
+
+def shifrator():
+        global new_word
+        step = int(input("Укажите шаг шифрования от 1 до 26\n"))
+        words = input("Введите фразу для шифрования \n:")
+        print()
+        for i in words:
+                if i in const_upper:
+                        num = const_upper.index(i)
+                        new_word.append(const_upper[num-step])
+                elif i in const_lower:
+                        num = const_lower.index(i)
+                        new_word.append(const_lower[num - step])
+                else:
+                        new_word.append(i)
+
+def deshifrator():
+        global post_word
+        step = int(input("Укажите шаг расшифрования от 1 до 26\n"))
+        unshif_word = input("Введите фразу для расшифрования \n")
+        for i in unshif_word:
+                if i in const_upper:
+                        num = const_upper.index(i)
+                        post_word.append(const_upper[num+step-26])
+                elif i in const_lower:
+                        num = const_lower.index(i)
+                        post_word.append(const_lower[num+step-26])
+                else:
+                        post_word.append(i)
+def listing():
+        global new_word
+        for i in new_word:
+                print(i, end="")
+        print()
+def postlisting():
+        global post_word
+        for i in post_word:
+                print(i, end="")
+        print()
+
+while True:
+        select = input("--->  ")
+        print()
+        if select == "0":
+                shifrator()
+        elif select == "1":
+                deshifrator()
+        elif select == "end":
+                break
+        elif select == "list":
+                listing()
+        elif select == "postlist":
+                postlisting()
