da7f7a97a5ba89f17c85784a67489d6da3bb222c
Kamil Haidarov <ckamil.haidarov@yandex.ru>
2025-01-09 22:01:05 +0300

    modified OOP_SPEC_METH

--- OOP_spec_meth.py
+++ OOP_spec_meth.py
@@ -17,26 +17,54 @@ class House:
     def __str__(self):
         return f"Название: {self.name}, кол-во этажей: {self.number_of_floors}"
     def __lt__(self, other):
-        return self.number_of_floors < other.number_of_floors
+        if isinstance(other, House):
+            return self.number_of_floors < other.number_of_floors
+        else:
+            return False
     def __gt__(self, other):
-        return self.number_of_floors > other.number_of_floors
+        if isinstance(other, House):
+            return self.number_of_floors > other.number_of_floors
+        else:
+            return False
     def __le__(self, other):
-        return self.number_of_floors <= other.number_of_floors
+        if isinstance(other, House):
+            return self.number_of_floors <= other.number_of_floors
+        else:
+            return False
     def __ge__(self, other):
-        return self.number_of_floors >= other.number_of_floors
+        if isinstance(other, House):
+            return self.number_of_floors >= other.number_of_floors
+        else:
+            return False
     def __ne__(self, other):
-        return self.number_of_floors != other.number_of_floors
+        if isinstance(other, House):
+            return self.number_of_floors != other.number_of_floors
+        else:
+            return False
     def __eq__(self, other):
-        return self.number_of_floors == other
+        if isinstance(other, House):
+            return self.number_of_floors == other
+        else:
+            return False
     def __add__(self, other):
-        self.number_of_floors = self.number_of_floors + other
-        return self
+        if isinstance(other, int):
+            self.number_of_floors = self.number_of_floors + other
+            return self
+        else:
+            return False
     def __iadd__(self, other):
-        self.number_of_floors += other
-        return self
+        if isinstance(other, int):
+            self.number_of_floors += other
+            return self
+        else:
+            return False
     def __radd__(self, other):
-        self.number_of_floors = other + self.number_of_floors
-        return self
+        if isinstance(other, int):
+            self.number_of_floors = other + self.number_of_floors
+            return self
+        else:
+            return False
+
 
 
 
